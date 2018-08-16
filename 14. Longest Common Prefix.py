2ef0488edb1c1b3be31c4a137f5f6c558dca2d00
libinglin <binglinli106@126.com>
2018-08-16 20:37:04 +0800

    leetcode 014

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -13,9 +13,11 @@ class Solution:
         """
         if not strs:
             return ""
+        # 若存在则一定在第一个字符串strs[0]中
         for i in range(len(strs[0])):
             for str in strs:
-                if len(str) < i or strs[0][i] != str[i]:
+                #存在比当前短的string或存在于当前index字符不相同的字符串
+                if len(str) <= i or strs[0][i] != str[i]:
                     return strs[0][:i]
         return strs[0]
     
