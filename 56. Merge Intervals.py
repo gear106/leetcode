7d4a425a427c82040397dc6658444e2c2452b2f6
libinglin <binglinli106@126.com>
2018-09-14 21:42:57 +0800

    leetcode056

---
+++ 56. Merge Intervals.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 14 21:39:38 2018
+
+@author: GEAR
+"""
+
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution:
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        res = []
+        for i in sorted(intervals, key=lambda x:x.start):
+            if res and i.start <= res[-1].end:
+                res[-1].end = max(res[-1].end, i.end)
+            else:
+                res.append(i)
+        return res
